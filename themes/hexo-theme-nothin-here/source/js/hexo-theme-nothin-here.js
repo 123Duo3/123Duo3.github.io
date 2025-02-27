029d6feec8c438adeae22ea8a3a93f6c27fb8f28
123Duo3 <123duo3@gmail.com>
2025-02-28 02:07:32 +0800

    feat(theme): Basically implemented the article page

--- themes/hexo-theme-nothin-here/source/js/hexo-theme-nothin-here.js
+++ themes/hexo-theme-nothin-here/source/js/hexo-theme-nothin-here.js
@@ -27,3 +27,35 @@ function getScrollbarHeight() {
 // 获取横向滚动条高度并设置 CSS 变量
 const scrollbarHeight = getScrollbarHeight();
 document.documentElement.style.setProperty('--scrollbar-height', `${scrollbarHeight}px`);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const postDates = document.querySelectorAll('.post-date');
+    postDates.forEach(function(postDateElement) {
+      const postDate = new Date(postDateElement.getAttribute('data-date'));
+      postDateElement.textContent = formatDate(postDate);
+    });
+  });
+
+const formatDate = function formatDate(postDate) {
+    const now = new Date();
+    const nowDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const postDateOnly = new Date(postDate.getFullYear(), postDate.getMonth(), postDate.getDate());
+    const diffDays = Math.floor((nowDate - postDateOnly) / (24 * 60 * 60 * 1000));
+  
+    if (diffDays === -2) {
+      return '后天';
+    } else if (diffDays === -1) {
+      return '明天';
+    } else if (diffDays === 0) {
+      return '今天';
+    } else if (diffDays === 1) {
+      return '昨天';
+    } else if (diffDays === 2) {
+      return '前天';
+    } else if (now.getFullYear() === postDate.getFullYear()) {
+      return `${postDate.getMonth() + 1} 月 ${postDate.getDate()} 日`;
+    } else {
+      return `${postDate.getFullYear()} 年 ${postDate.getMonth() + 1} 月 ${postDate.getDate()} 日`;
+    }
+}
